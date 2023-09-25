830486e1e10a63aeea02872f5615daab5fa897ab
skyinr <2587135207@qq.com>
2023-09-25 19:04:59 +0800

    方块模型生成修改

--- src/main/java/com/skyinr/datagendemo/datagen/ModBlockStateProvider.java
+++ src/main/java/com/skyinr/datagendemo/datagen/ModBlockStateProvider.java
@@ -15,6 +15,15 @@ public class ModBlockStateProvider extends BlockStateProvider {
     protected void registerStatesAndModels() {
         simpleBlock(ModBlocks.BLOCK_DEMO.get());
 
+        //读者可以选择使用把某些方块抽象成一个接口来批量生成
+        //如下例
+        /*
+        Collection<RegistryObject<Block>> entries = ModBlocks.BLOCKS.getEntries();
+        entries.stream().map(RegistryObject::get)
+                .filter(block -> block instanceof RotatedPillarBlock)
+                .map(block -> (RotatedPillarBlock)block)
+                .forEach(this::logBlock);
+         */
     }
 
 }
